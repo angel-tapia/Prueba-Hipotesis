3a4d44b9f4b0b7e3ee91ff12edc26cf2d81a5b86
Emiliano García <emiliano.garciadavila@hotmail.com>
2022-11-20 19:40:00 -0600

    Prueba de hipótesis concernite a varianzas

--- Tablas.py
+++ Tablas.py
@@ -3,8 +3,8 @@ from IPython.display import display
 
 # Leemos el excel con los datos
 tablaZ = pd.read_excel(r"Prueba-Hipotesis\TablaZ.xlsx")
+tablaT = pd.read_excel(r"Prueba-Hipotesis\TablaT.xlsx")
 tablaJiCuadrada = pd.read_excel(r"Prueba-Hipotesis\TablaJiCuadrada.xlsx")
-#tablaT = pd.read_excel(r"Prueba-Hipotesis\TablaT.xlsx")
 #tablaFisher = pd.read_excel(r"Prueba-Hipotesis\TablaFisher.xlsx")
 
 """
@@ -35,6 +35,10 @@ def buscarZ(x):
     <param name="n">Grados de libertad.</param>
     <return>Valor de la tabla JiCuadrada.</return>
 """
+
+def buscarT(alpha, n):
+    return tablaT[alpha][n-1]
+
 def buscarJiCuadrada(alpha, n):
     return tablaJiCuadrada[alpha][n-1]
     
--- Varianza.py
+++ Varianza.py
@@ -9,4 +9,64 @@ import os
     </summary>
 """
 def menu_varianza():
+    print("Elige la prueba de hipotesis a utilizar:")
+    print("1.")
+    print("H0 " + Ssigma + " = " + Ssigma + "_0")
+    print("H1 " + Ssigma + " < " + Ssigma + "_0")
+    print("2.")
+    print("H0 " + Ssigma + " = " + Ssigma + "_0")
+    print("H1 " + Ssigma + " > " + Ssigma + "_0")
+    print("3.")
+    print("H0 " + Ssigma + " = " + Ssigma + "_0")
+    print("H1 " + Ssigma + " != " + Ssigma + "_0")
+    opcion = int(input("Opcion: "))
+    os.system("cls")
+    if opcion not in [1, 2, 3]:
+        print("Opcion incorrecta, vuelve a intentarlo.")
+        print("Presione cualquier tecla para continuar...")
+        os.system("pause")
+        os.system("cls")
+        menu_varianza()
+        return
+    
+    print("Prueba de hipotesis concernite a varianzas")
+    print("H0 " + Ssigma + " = " + Ssigma + "_0")
+    print("H1 " + Ssigma + " " + ["<", ">", "!="][opcion - 1] + " " + Ssigma + "_0")
+    print("")
+    print("Introduce los datos:")
+    n = int(input("n: "))
+    varmu = float(input("S: "))
+    sigma = float(input(Ssigma + ": "))
+    alpha = float(input(Salpha + ": "))
+    chi = (n - 1)(math.sqrt(varmu)) / (math.sqrt(sigma))
+    print(Schi+"^2 = " + str(chi))
+    prueba(chi, ["<", ">", "!="][opcion - 1], alpha, n-1)
     return
+
+"""
+    <summary>
+        Funcion que rechaza o no rechaza H0.
+    </summary>
+    <param name="z">Valor del estadistico prueba.</param>
+    <param name="operation">Operacion a realizar.</param>
+    <param name="alpha">Valor de alpha.</param>
+"""
+def prueba(chi, operation, alpha, n):
+    if operation == "<":
+        if chi < buscarJiCuadrada(1-alpha, n):
+            print("Rechazamos H0")
+        else:
+            print("No rechazamos H0")
+    elif operation == ">":
+        if chi > buscarJiCuadrada(alpha, n):
+            print("Rechazamos H0")
+        else:
+            print("No rechazamos H0")
+    elif operation == "!=":
+        if chi < buscarJiCuadrada((1-alpha) / 2, n) or chi > buscarZ(alpha / 2, n):
+            print("Rechazamos H0")
+        else:
+            print("No rechazamos H0")
+    return
+    
+menu_varianza()
