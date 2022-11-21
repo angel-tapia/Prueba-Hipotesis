6e5f578b60fc77e1c04512f94024cab5e403c712
angel-tapia <angeltapia1805@gmail.com>
2022-11-21 11:10:22 -0600

    changes

--- Tablas.py
+++ Tablas.py
@@ -5,7 +5,8 @@ from IPython.display import display
 tablaZ = pd.read_excel(r"Prueba-Hipotesis\TablaZ.xlsx")
 tablaJiCuadrada = pd.read_excel(r"Prueba-Hipotesis\TablaJiCuadrada.xlsx")
 #tablaT = pd.read_excel(r"Prueba-Hipotesis\TablaT.xlsx")
-#tablaFisher = pd.read_excel(r"Prueba-Hipotesis\TablaFisher.xlsx")
+tablaFisher01 = pd.read_excel(r"Prueba-Hipotesis\TablaF0.01.xlsx")
+#tablaFisher05 = pd.read_excel(r"Prueba-Hipotesis\TablaF0.005.xlsx")
 
 """
     <summary>
@@ -37,4 +38,42 @@ def buscarZ(x):
 """
 def buscarJiCuadrada(alpha, n):
     return tablaJiCuadrada[alpha][n-1]
-    
+
+"""
+    <summary>
+        Funcion que calcula el valor de la tabla Fisher 0.01.
+    </summary>
+    <param name="v1">Grados de libertad.</param>
+    <param name="v2">Grados de libertad.</param>
+    <return>Valor de la tabla Fisher 0.01.</return>
+"""
+def buscarFisher01(v1, v2):
+    return tablaFisher01[v1][v2]
+
+"""
+    <summary>
+        Funcion que calcula el valor de la tabla Fisher 0.05.
+    </summary>
+    <param name="v1">Grados de libertad.</param>
+    <param name="v2">Grados de libertad.</param>
+    <return>Valor de la tabla Fisher 0.05.</return>
+"""
+def buscarFisher05(v1, v2):
+    return #tablaFisher05[v1][v2]
+
+"""
+    <summary>
+        Funcion que calcula el valor de la tabla Fisher.
+    </summary>
+    <param name="v1">Grados de libertad.</param>
+    <param name="v2">Grados de libertad.</param>
+    <param name="alpha">Valor de la significancia.</param>
+    <return>Valor de la tabla Fisher.</return>
+"""
+def buscarF(v1, v2, alpha):
+    if alpha == 0.01:
+        return buscarFisher01(v1, v2)
+    else:
+        return buscarFisher05(v1, v2)
+
+display(tablaFisher01)
